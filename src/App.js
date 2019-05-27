7860b6a750d3a2bc31f2a70b2c495a8856964d07
Aleksandar Ristic <storiaca@gmail.com>
2019-05-27 16:14:02 +0200

    Show recipe on search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./components/Form";
+
+const API_KEY = "26a279f32f501853292bf686e9ebbe96";
 
 class App extends Component {
+  state = {
+    recipes: []
+  };
+  getRecipe = async e => {
+    e.preventDefault();
+    const recipeName = e.target.elements.recipeName.value;
+
+    const api_call = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`
+    );
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  };
   render() {
+    const { recipes } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe} />
+        {recipes.map(recipe => {
+          return <p key={recipe.recipe_id}>{recipe.title}</p>;
+        })}
       </div>
     );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import PropTypes from "prop-types";
+const Form = props => {
+  return (
+    <form onSubmit={props.getRecipe}>
+      <input type="text" name="recipeName" />
+      <button>Search</button>
+    </form>
+  );
+};
+
+export default Form;
