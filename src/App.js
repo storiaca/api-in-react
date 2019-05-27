3dae5649c0891acc41c6246d1d8b784493f5fcc0
Aleksandar Ristic <storiaca@gmail.com>
2019-05-27 16:30:23 +0200

    Pass props to Recipies component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Form from "./components/Form";
+import Recipes from "./components/Recipes";
 
 const API_KEY = "26a279f32f501853292bf686e9ebbe96";
 
@@ -13,23 +14,21 @@ class App extends Component {
     const recipeName = e.target.elements.recipeName.value;
 
     const api_call = await fetch(
-      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`
+      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
     );
     const data = await api_call.json();
     this.setState({ recipes: data.recipes });
     console.log(this.state.recipes);
   };
   render() {
-    const { recipes } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
-        {recipes.map(recipe => {
-          return <p key={recipe.recipe_id}>{recipe.title}</p>;
-        })}
+
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
