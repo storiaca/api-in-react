037ade018d6b6cbcd2d66b7d486aa96ebb94f5eb
Aleksandar Ristic <storiaca@gmail.com>
2019-05-29 15:26:47 +0200

    Save data to local storage

--- src/App.js
+++ src/App.js
@@ -20,6 +20,17 @@ class App extends Component {
     this.setState({ recipes: data.recipes });
     console.log(this.state.recipes);
   };
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({
+      recipes
+    });
+  };
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  };
   render() {
     return (
       <div className="App">
